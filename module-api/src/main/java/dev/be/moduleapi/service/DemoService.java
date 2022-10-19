eae58c135ca04b01c381163a37d570eef33f91d1
JayPark7821 <pjhyun1990@gmail.com>
2022-10-19 19:29:15 +0900

    hello world multi module

---
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -0,0 +1,40 @@
+package dev.be.moduleapi.service;
+
+import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
+import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repository.MemberRepository;
+import dev.be.modulecommon.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DemoService {
+
+    @Value("${profile-name}")
+    private String profile;
+    private final CommonDemoService commonDemoService;
+    private final MemberRepository memberRepository;
+    
+    public String save() {
+        System.out.println("profile = " + profile);
+        memberRepository.save(Member.builder()
+                .name(Thread.currentThread().getName())
+                .build());
+        return "save";
+    }
+
+    public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println("size = " + size);
+        return "find";
+    }
+
+    public String exception() {
+        throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+//        return "exception";
+    }
+}
+
